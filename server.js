d58e904321f010a12d71355675c90ea7417f3052
1OneGroup <erp@onecity.in>
2023-03-03 11:34:40 +0530

    are thik hoja

--- server.js
+++ server.js
@@ -3,36 +3,32 @@ const cors = require("cors");
 const { WorkOrder } = require("./config");
 
 const app = express();
-app.use(express.json()); // use express.json() middleware to parse incoming JSON data
+// app.use(express.json()); // use express.json() middleware to parse incoming JSON data
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 app.post("/addworkorder", async (req, res) => {
-  // const data = req.body;
+  const data = req.body;
 
-  // const newWorkOrder = {
-  //   Date: new Date(),
-  //   workOrderDate: data.workOrderDate,
-  //   Amount: data.Amount,
-  //   vendor: data.vendor,
-  //   woNo: data.woNo,
-  // };
+  const newWorkOrder = {
+    Date: new Date(),
+    workOrderDate: data.workOrderDate,
+    Amount: data.Amount,
+    vendor: data.vendor,
+    woNo: data.woNo,
+  };
 
-  // console.log(newWorkOrder);
+  console.log(newWorkOrder);
 
-  // try {
-  //   await WorkOrder.add(newWorkOrder);
-  //   console.log("Work order added successfully");
-  //   res.send("Work order added successfully");
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).send("Error adding work order to database");
-  // }
-  console.log(req)
-    res.send(req);
-
-
+  try {
+    await WorkOrder.add(newWorkOrder);
+    console.log("Work order added successfully");
+    res.send("Work order added successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding work order to database");
+  }
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
